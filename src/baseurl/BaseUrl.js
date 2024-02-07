13662728abef1ff993a19a5d5d5ff1afad4e5c8c
Muhammad Minhaj <mfa0316@gmail.com>
2024-02-07 20:19:40 +0500

    lll

--- src/baseurl/BaseUrl.js
+++ src/baseurl/BaseUrl.js
@@ -1,46 +1,6 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-
-    
-    // baseUrl:"https://api.valet-parking.ga/",
-
-    
-    // baseUrl:"https://projectvethomes.pythonanywhere.com/",
-    // imgUrl:"https://projectvethomes.pythonanywhere.com",
-    // baseUrl:"https://api.askejren.ml/",
-    // imgUrl:"https://api.askejren.ml/",
-    
-    // baseUrl:"http://167.114.96.66:8000/",
-    // imgUrl:"http://167.114.96.66:8000",
     baseUrl:"https://pscapi.devssh.xyz/api/",
-    // imgUrl:"https://apivethomes.devssh.xyz",
-    // imgUrl2:"https://apivethomes.devssh.xyz",
-    // imgUrl1:"https://apivethomes.devssh.xyz/media/",
-    // // baseUrl:"http://192.168.0.109:8000/",
-    // imgUrl:"http://192.168.0.109:8000/",
-    // imgUrl1:"http://192.168.0.109:8000/media/",
-    // imgUrl2:"http://192.168.0.109:8000",
-
-
-
-    
-
-    //  baseUrl:"http://127.0.0.1:8000/",
-    // imgUrl:"http://127.0.0.1:8000/media/",
-    
-    
-    // baseUrl:"https://api.thecnchub.com/webapi",
-    // imgUrl:"https://api.thecnchub.com/media/"
-
-    // baseUrl:"https://api.thecnchub.com/webapi",
-    // imgUrl:"https://api.thecnchub.com/media/"
-
-
-    
-
-  
-
-    
-
-}
+   }
 
 // http://cnchub.pythonanywhere.com/webapi
